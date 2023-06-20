bae143984917d3521fec47b401918d6d34deb0a6
sivaterraform <137068733+sivaterraform@users.noreply.github.com>
2023-06-20 10:58:24 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAWWACO5SDWT3JLVOR"
+    secret_key = "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"
 
-terraform {
-  required_version = ">=0.12"
+}
+module "VPC" {
+  source = ".\\vpc"
 }
 
-resource "aws_vpc" "mumvpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "mumvpc"
-  }
+module "SUBNET" {
+  source = ".\\subnet"
 }
+
+module "IGANDROUTE" {
+  source = ".\\igandroute"
+}
+
+module "SECURITYGROUP" {
+  source = ".\\securitygroup"
+}
+
+module "EC2INSTANCE" {
+  source = ".\\ec2instance"
+}
+
+
+
+
